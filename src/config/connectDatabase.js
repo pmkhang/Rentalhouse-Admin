8a0158c73e2defc8907820ce78e71cca0d02337b
Pham Minh Khang <pham.m.khang.98@gmail.com>
2023-09-26 18:30:30 +0700

    .

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -1,18 +1,24 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('pmkhang', 'root', null, {
-   host: 'localhost',
-   dialect: 'mysql',
-   logging: false,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const connectDatabase = async () => {
-   try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-   } catch (error) {
-      console.error('Unable to connect to the database:', error);
-   }
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 };
 
 export default connectDatabase;
